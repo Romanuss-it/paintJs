35f67f3622ec1e1b7416ea86e9e09aaf7336b740
romanuss_ <roman.bashtovyy@gmail.com>
2021-12-08 12:16:41 +0300

    change color

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+
+canvas.height = 700;
+canvas.width = 700;
 
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = '#2c2c2c';
@@ -31,7 +35,10 @@ function onMouseDown (event) {
     painting = true;
 }
 
-
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
 
 
 if (canvas) {
@@ -40,3 +47,5 @@ if (canvas) {
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
